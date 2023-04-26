d998d9cc03476e7cbc6cf0f2e804b4c1976d6f92
João Augusto <112037149+JaoFnz135@users.noreply.github.com>
2023-04-25 22:32:43 -0300

    Update BancoDeDados.py

--- BancoDeDados.py
+++ BancoDeDados.py
@@ -110,11 +110,52 @@ def pesquisarTransacao():
 
     tabela_janela.mainloop()
 
+def visualizar_historico():
+    conn = psycopg2.connect(database="mnlfnrwe", 
+                            user="mnlfnrwe", 
+                            password="RnSYVKvtLjKAF5SqXPJlll0AuNveFDO_", 
+                            host="kesavan.db.elephantsql.com", 
+                            port="5432")
+    # Cria um cursor(objeto) para manipular o BD
+    cur = conn.cursor()
+    # O método .execute("[Codigo SQL]") manipula o BD
+    cur.execute("SELECT * FROM investimentos ORDER BY dt_transacao ASC")
+    # Obtém os dados do banco de dados
+    dados = cur.fetchall()
+
+    # Cria uma nova janela para exibir a tabela
+    tabela_janela = tk.Toplevel()
+    tabela_janela.geometry('1920x1080')
+    tabela_janela.title("Investimentos")
+    
+    # Criando uma tabela
+    tabela = tk.Label(tabela_janela, text="Investimentos", font=("Helvetica", 16, "bold"))
+    tabela.grid(row=0, column=0, columnspan=6, padx=10, pady=10)
+    
+    # Criando as colunas
+    colunas = ["Cód. Transação", "Código", "Data", "Quantidade", "Valor Unitário", "Taxa de Corretagem", "Tipo Transação", "Valor Operação", "Imposto", "Valor Total"]
+    for i in range(len(colunas)):
+        coluna_label = tk.Label(tabela_janela, text=colunas[i], font=("Helvetica", 12, "bold"))
+        coluna_label.grid(row=1, column=i, padx=10, pady=10)
+
+    # Exibe os dados na tabela
+    for i in range(len(dados)):
+        for j in range(len(dados[i])):
+            dado_label = tk.Label(tabela_janela, text=str(dados[i][j]), font=("Helvetica", 12))
+            dado_label.grid(row=i+2, column=j, padx=10, pady=5)
+            
+            
+    # Encerra a conexão com o banco de dados
+    cur.close()
+    conn.close()
+
+    tabela_janela.mainloop()
 
 def main():
-    pesquisarAtivo()
-    
-    
+    #pesquisarAtivo()
+    #pesquisarTransacao()
+    #visualizar_historico()
+    pass
     
 if __name__ == '__main__':
     main()
